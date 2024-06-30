d629d7cc8addb5efad43249b25c3f379e4072396
pchee <pchee1489@naver.com>
2024-07-01 06:53:02 +0900

    메인화면 설계

--- app.vue
+++ app.vue
@@ -14,8 +14,8 @@
             </div>
           </div>
           <!-- Main content -->
-          <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
-            <h1>asd</h1>
+          <main class="col-span-12 marker:col-span-8 xl:col-span-6">
+            <router-view></router-view>
           </main>
           <!-- Right sidebar -->
           <div
