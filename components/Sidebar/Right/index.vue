45de808ccc60f5a03864c1eb3970d56f827f56f6
pchee <pchee1489@naver.com>
2024-07-01 06:10:29 +0900

    오른쪽사이드바 화면설계

---
+++ components/Sidebar/Right/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="flex flex-col">
+    <!-- Preview Card : What's happening -->
+    <SidebarRightPreviewCard title="What's happening">
+      <SidebarRightPreviewCardItem
+        v-for="item in whatsHappeningItems"
+        :key="item.title"
+      >
+        <div>
+          <h2 class="font-bold text-gray-800 text-md dark:text-white">
+            {{ item.title }}
+          </h2>
+          <p class="text-xs text-gray-400">{{ item.count }}</p>
+        </div>
+      </SidebarRightPreviewCardItem>
+    </SidebarRightPreviewCard>
+
+    <!-- Preview Card : Who to follow -->
+    <SidebarRightPreviewCard title="Who to follow">
+      <SidebarRightPreviewCardItem
+        v-for="item in whoToFollowItems"
+        :key="item.name"
+      >
+        <div class="flex flex-row justify-between items-center p-2">
+          <div class="flex flex-row">
+            <img
+              class="w-10 h-10 rounded-full"
+              :src="item.image"
+              :alt="item.name"
+            />
+            <div class="flex flex-col ml-2">
+              <h1 class="text-sm font-bold text-gray-900 dark:text-white">
+                {{ item.name }}
+              </h1>
+              <p class="text-xs text-gray-400">{{ item.handle }}</p>
+            </div>
+          </div>
+          <div class="flex h-full">
+            <button
+              class="px-4 py-2 font-bold text-xs text-white dark:text-black bg-black dark:bg-white rounded-full"
+            >
+              Follow
+            </button>
+          </div>
+        </div>
+      </SidebarRightPreviewCardItem>
+    </SidebarRightPreviewCard>
+  </div>
+</template>
+
+<script setup>
+const whatsHappeningItems = ref([
+  {
+    title: "SpaceX",
+    count: "18.8k Tweets",
+  },
+  {
+    title: "#CodingIsFun",
+    count: "8.8k Tweets",
+  },
+  {
+    title: "#artforall",
+    count: "1.8k Tweets",
+  },
+]);
+
+const whoToFollowItems = ref([
+  {
+    name: "Joe Biden",
+    handle: "@joeBiden",
+    image: "https://picsum.photos/200/200",
+  },
+  {
+    name: "Joe Biden",
+    handle: "@joeBiden",
+    image: "https://picsum.photos/200/200",
+  },
+  {
+    name: "Joe Biden",
+    handle: "@joeBiden",
+    image: "https://picsum.photos/200/200",
+  },
+]);
+</script>
